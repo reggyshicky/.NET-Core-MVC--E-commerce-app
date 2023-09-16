04fc9ecd0e7fdb20e95416c0c5cd183f438877ad
Reggy Shicky <reginahshikanda@gmail.com>
2023-09-16 03:24:50 +0300

    Responsive UI, Implemented Create CRUD operation, made a correspinding Create.cshtml for the View purposes

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -16,5 +16,9 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
