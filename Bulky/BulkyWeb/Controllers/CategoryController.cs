6becc0d8fcf039fedbf90003c6e58168d470c08f
Reggy Shicky <reginahshikanda@gmail.com>
2023-09-16 23:33:55 +0300

    Get and Post method to facilitate creation of a category,use the same IActionResult return type method

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,11 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Intrinsics.X86;
+using System;
+using Microsoft.Extensions.Hosting;
+using static System.Collections.Specialized.BitVector32;
+using System.Reflection.Metadata;
 
 namespace BulkyWeb.Controllers
 {
@@ -16,8 +21,31 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        //This is an HTTP GET request handler.It returns a view for creating a new category.In other words, when a user navigates to a URL or 
+        //clicks a link that maps to this action, they will see a form or page for creating a new category.
         public IActionResult Create()
         {
+            return View();
+        }
+
+
+        //This is an HTTP POST request handler.It is used to handle the submission of the form created in the Create GET action.When a user 
+        //submits the form, the data is sent as an HTTP POST request to this action.
+        
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid) 
+            {
+                _db.Categories.Add(obj); //keeping track of what it has to add
+                _db.SaveChanges(); //go to the db and create that category
+                return RedirectToAction("Index", "Category");
+            }
             return View();
         }
     }
--- Bulky/BulkyWeb/Models/Category.cs
+++ Bulky/BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -7,7 +8,11 @@ namespace BulkyWeb.Models
         [Key] //Data anotation, id is the primary key of our table
         public int Id { get; set; }
         [Required] //Not null
+        [DisplayName("Category Name")]
+        [MaxLength(30)] //validation
         public string Name { get; set; }
+        [DisplayName("Display Order")] //Data anotation for the client side UI , the name DisplayOrder will have a space in btn; Display Order
+        [Range(1,100,ErrorMessage="Display Order must be between 1-100")] //validation
         public int DisplayOrder { get; set; } //which category should be display 1st on the page
     }
 }
