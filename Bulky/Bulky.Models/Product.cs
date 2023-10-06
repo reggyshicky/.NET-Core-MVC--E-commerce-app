4594e4fdd06a7d635800ba10329faf74e5ec352f
Reggy Shicky <reginahshikanda@gmail.com>
2023-10-07 01:11:28 +0300

    Created the Product Model and assigned properties

---
+++ Bulky/Bulky.Models/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 1000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 1000)]
+        public double Price50{ get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+
+    }
+}
+
+//In various applications, especially in e-commerce or product catalog systems, the list price is the price at which a product is 
+//initially listed or displayed before any discounts, promotions, or special pricing is applied.
