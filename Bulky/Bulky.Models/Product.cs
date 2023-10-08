067e7c04c2346b1a4d3acc2d2f0cd78fcf5f7925
Reggy Shicky <reginahshikanda@gmail.com>
2023-10-08 20:34:14 +0300

    Registered the Product table in the ApplicationDBContext and seeded the data

--- Bulky/Bulky.Models/Product.cs
+++ Bulky/Bulky.Models/Product.cs
@@ -13,6 +13,7 @@ namespace Bulky.Models
         public int Id { get; set; }
         [Required]
         public string Title { get; set; }
+        public string Description { get; set; }
         [Required]
         public string ISBN { get; set; }
         [Required]
