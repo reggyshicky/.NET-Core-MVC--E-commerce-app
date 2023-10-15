006eff62259d6bbf08e605882328c53d93b3d786
Reggy Shicky <reginahshikanda@gmail.com>
2023-10-15 10:52:41 +0300

    Details UI

--- Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -23,7 +23,11 @@ namespace BulkyWeb.Areas.Customer.Controllers
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
             return View(productList);
         }
-
+        public IActionResult Details(int? productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id==productId, includeProperties: "Category");
+            return View(product);
+        }
         public IActionResult Privacy()
         {
 
