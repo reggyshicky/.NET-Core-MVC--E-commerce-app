2f213272e3a21989601489528f47500ea2812c23
Reggy Shicky <reginahshikanda@gmail.com>
2023-10-16 19:00:43 +0300

    Tokenproviders  to enable password reset, email confirmation and 2FA

--- Bulky/BulkyWeb/Program.cs
+++ Bulky/BulkyWeb/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
@@ -42,6 +42,20 @@ app.MapControllerRoute(
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-        
- 
+
+
+
+
+
+
+//One of the features provided by the Identity system is token-based authentication. Tokens are used for various features such as:
+
+//Confirming a user's email address
+//Resetting a user's password
+//Implementing two-factor authentication
+//These features require the generation and management of tokens. The AddDefaultTokenProviders() method is used to configure Identity to use the default token providers for these features.
+
+//When you call AddDefaultTokenProviders(), you are essentially telling Identity to use its built-in token providers. These providers handle the creation, validation, and storage of the tokens required for the aforementioned features.
+
+
 
