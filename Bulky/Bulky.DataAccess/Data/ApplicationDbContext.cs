17828132cdba60ef21a6d354d6122c268de819cd
Reggy Shicky <reginahshikanda@gmail.com>
2023-10-22 23:19:52 +0300

    Creating OrderHeader and OrderDetails classes

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -15,8 +15,11 @@ namespace Bulky.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
        
 
         //seeding data
