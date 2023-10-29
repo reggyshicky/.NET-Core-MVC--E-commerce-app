b119025a46e11a7b9750eba9f9f45ff93f297b76
Reggy Shicky <reginahshikanda@gmail.com>
2023-10-29 22:49:06 +0300

    OrderDetails get Action

--- Bulky/BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,7 +1,10 @@
 ﻿using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
@@ -18,12 +21,43 @@ namespace BulkyWeb.Areas.Admin.Controllers
 		{
 			return View();
 		}
-		#region APICALLS
-		[HttpGet]
-		public IActionResult GetAll()
+        public IActionResult Details(int orderId)
+        {
+            OrderVM orderVM= new()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+            };
+            return View(orderVM);
+        }
+        //_unitOfWork is typically an instance of a unit of work class, which is often used to manage data access operations.
+        //.OrderHeader refers to a repository or service for accessing order header data within the unit of work.
+        //A repository is a design pattern in software development that provides an abstraction layer between the application's business logic and the data source, such as a database or
+        //an external service. It's used to centralize data access and decouple the application from the specific details of how data is stored and retrieved. Repositories offer several
+        //benefits, including code organization, maintainability, and testability.
+        #region APICALLS
+        [HttpGet]
+		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-			return Json(new { data = objOrderHeaders });
+			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+            return Json(new { data = objOrderHeaders });
 		}
 
 
--- Bulky/BulkyWeb/wwwroot/js/order.js
+++ Bulky/BulkyWeb/wwwroot/js/order.js
@@ -1,32 +1,55 @@
-var dataTable
+var dataTable;
 
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
+
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/order/getall' },
+        "ajax": { url: '/admin/order/getall?status=' + status },
         "columns": [
             { data: 'id', "width": "5%" },
-            { data: 'name', "width": "15%" },
+            { data: 'name', "width": "25%" },
             { data: 'phoneNumber', "width": "20%" },
-            { data: 'applicationUser.email', "width": "15%" },
+            { data: 'applicationUser.email', "width": "20%" },
             { data: 'orderStatus', "width": "10%" },
             { data: 'orderTotal', "width": "10%" },
             {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                    <a href="/admin/order/details?OrderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>
+                     <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>               
+                    
                     </div>`
                 },
-                "width": "15%"
+                "width": "10%"
             }
         ]
     });
 }
 
 
-
+/*windows.location represents the current URL of the web page and .search returns the query string part of the URL*/
 /*"ajax" specifies that you want to use an external data source to populate the DataTable.*/
