7be9bbb2169ed14ca326f738cd20947bd447463c
Reggy Shicky <reginahshikanda@gmail.com>
2023-10-30 10:15:44 +0300

    Update order details

--- Bulky/BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using Bulky.Models;
 using Bulky.Models.ViewModels;
 using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -13,6 +14,8 @@ namespace BulkyWeb.Areas.Admin.Controllers
 	{
 		
 		private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
+        public OrderVM OrderVM { get; set; }
         public OrderController(IUnitOfWork unitOfWork)
         {
 			_unitOfWork = unitOfWork;
@@ -23,18 +26,48 @@ namespace BulkyWeb.Areas.Admin.Controllers
 		}
         public IActionResult Details(int orderId)
         {
-            OrderVM orderVM= new()
+            OrderVM= new()
             {
                 OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
                 OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
             };
-            return View(orderVM);
+            return View(OrderVM);
         }
         //_unitOfWork is typically an instance of a unit of work class, which is often used to manage data access operations.
         //.OrderHeader refers to a repository or service for accessing order header data within the unit of work.
         //A repository is a design pattern in software development that provides an abstraction layer between the application's business logic and the data source, such as a database or
         //an external service. It's used to centralize data access and decouple the application from the specific details of how data is stored and retrieved. Repositories offer several
         //benefits, including code organization, maintainability, and testability.
+
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin+","+SD.Role_Employee)]
+        public IActionResult UpdateOrderDetail(int orderId)
+        {
+            var orderHeaderFromDb = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id);
+            orderHeaderFromDb.Name = OrderVM.OrderHeader.Name;
+            orderHeaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
+            orderHeaderFromDb.StreetAddress = OrderVM.OrderHeader.StreetAddress;
+            orderHeaderFromDb.City = OrderVM.OrderHeader.City;
+            orderHeaderFromDb.State = OrderVM.OrderHeader.State;
+            orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
+            if (!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier))
+            {
+                orderHeaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
+            }
+            if (!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber))
+            {
+                orderHeaderFromDb.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            }
+            _unitOfWork.OrderHeader.Update(orderHeaderFromDb);
+            _unitOfWork.Save();
+            TempData["Success"] = "Order Details Updated Succesfully.";
+            return RedirectToAction(nameof(Details), new {orderId=orderHeaderFromDb.Id});
+        }
+       
+
+
+
         #region APICALLS
         [HttpGet]
 		public IActionResult GetAll(string status)
